2491431899122726b6f952803501168c2245fed2
rafliandi13 <herp1306@gmail.com>
2020-12-17 18:17:11 +0700

    rapiin kodingan

--- app/src/main/java/com/example/pelayananonlinesumedang/ui/Beranda/BerandaFragment.java
+++ app/src/main/java/com/example/pelayananonlinesumedang/ui/Beranda/BerandaFragment.java
@@ -17,7 +17,6 @@ public class BerandaFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_beranda, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
         return root;
     }
 }
