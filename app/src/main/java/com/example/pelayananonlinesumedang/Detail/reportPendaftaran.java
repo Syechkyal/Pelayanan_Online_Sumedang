e94e7f938ae0b9f568e4e87899d731568dcbfc18
Muhammad Febriansyah <mfebriiii21@gmail.com>
2020-12-18 18:20:39 +0700

    Membuat report pendaftaran

---
+++ app/src/main/java/com/example/pelayananonlinesumedang/Detail/reportPendaftaran.java
@@ -0,0 +1,16 @@
+package com.example.pelayananonlinesumedang.Detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pelayananonlinesumedang.R;
+
+public class reportPendaftaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report_pendaftaran);
+    }
+}
