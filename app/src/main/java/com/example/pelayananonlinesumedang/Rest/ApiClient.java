7572d057afdb05020c959d2b022234db466858c4
Syechkyal <syechkyal@gmail.com>
2020-12-23 23:48:44 +0700

    fragmenpendaftaran

--- app/src/main/java/com/example/pelayananonlinesumedang/Rest/ApiClient.java
+++ app/src/main/java/com/example/pelayananonlinesumedang/Rest/ApiClient.java
@@ -1,22 +1,7 @@
 package com.example.pelayananonlinesumedang.Rest;
 
-import androidx.constraintlayout.solver.widgets.Chain;
-
-import com.example.pelayananonlinesumedang.Model.GetJenisPelayanan;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.List;
-
-import okhttp3.Headers;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
--- app/src/main/java/com/example/pelayananonlinesumedang/ui/Pendaftaran/PendaftaranFragment.java
+++ app/src/main/java/com/example/pelayananonlinesumedang/ui/Pendaftaran/PendaftaranFragment.java
@@ -1,6 +1,8 @@
 package com.example.pelayananonlinesumedang.ui.Pendaftaran;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,11 +11,13 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import com.example.pelayananonlinesumedang.Model.GetJenisPelayanan;
@@ -23,7 +27,9 @@ import com.example.pelayananonlinesumedang.Rest.ApiClient;
 import com.example.pelayananonlinesumedang.Rest.ApiInterface;
 import com.example.pelayananonlinesumedang.Rest.UtilsApi;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import butterknife.BindView;
@@ -38,8 +44,11 @@ public class PendaftaranFragment extends Fragment {
     ApiInterface apiInterface;
     EditText edt_nama;
     EditText edt_nik;
-    EditText tgl_booking;
     Button btn_daftar;
+    EditText Tx_tanggal_booking;
+    DatePickerDialog datePickerDialog;
+    SimpleDateFormat dateFormatter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_pendaftaran, container, false);
@@ -48,7 +57,16 @@ public class PendaftaranFragment extends Fragment {
 
         edt_nama = root.findViewById(R.id.edt_nama);
         edt_nik = root.findViewById(R.id.edt_nik);
-        tgl_booking = root.findViewById(R.id.edt_tanggal);
+
+        Tx_tanggal_booking = root.findViewById(R.id.txTanggal);
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+        Tx_tanggal_booking.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                showDateDialog();
+            }
+        });
         btn_daftar = root.findViewById(R.id.button_submit);
         spinner = root.findViewById(R.id.list_form_pelayanan);
         ButterKnife.bind(getActivity());
@@ -77,8 +95,8 @@ public class PendaftaranFragment extends Fragment {
                     edt_nama.setError("Masukan Nama Dengan Benar");
                 }if(edt_nik.getText().toString().length() == 0){
                     edt_nik.setError("Masukan NIK Dengan Benar");
-                }if (tgl_booking.getText().toString().length() == 0){
-                    tgl_booking.setError("Masukan Tanggal Dengan Benar");
+                }if (Tx_tanggal_booking.getText().toString().length() == 0){
+                    Tx_tanggal_booking.setError("Masukan Tanggal Dengan Benar");
                 }else{
 
                     Toast.makeText(getActivity(), "Registrasi Berhasil!", Toast.LENGTH_SHORT).show();
@@ -90,6 +108,22 @@ public class PendaftaranFragment extends Fragment {
         getActivity().setTitle("Pendaftaran Layanan");
         return root;
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void showDateDialog(){
+        Calendar calendar = Calendar.getInstance();
+
+        datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                Tx_tanggal_booking.setText(dateFormatter.format (newDate.getTime()));
+            }
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+        }
+
     void initSpinnerJenisLayanan(){
         apiInterface.getJenisPelayanan("Basic bW9iaWxlOjFudDNuNS4yMDIw").enqueue(new Callback<GetJenisPelayanan>() {
             @Override
